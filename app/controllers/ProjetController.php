ec7b3bcb7ec88d46ffd6fd7c390738caa8cdd843
ErwanGesquin <gesquinerwan@hotmail.fr>
2015-03-17 16:45:11 +0100

    modification de la vue

--- app/controllers/ProjetController.php
+++ app/controllers/ProjetController.php
@@ -4,7 +4,6 @@ class ProjetController extends ControllerBase
 {
 	public function indexAction($id){
 		$equipe = User::find(array("id" => $id));
-		$projet = Projet::find(array("idClient => $id"));
 		$this->view->setVar("equipe", $equipe);
 	}
 
