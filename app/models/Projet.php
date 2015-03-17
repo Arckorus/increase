dbe4118d818222bdc43edf6a2f1828388a8e7f52
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-17 11:35:17 +0100

    ajout de la liste des projets

--- app/models/Projet.php
+++ app/models/Projet.php
@@ -61,4 +61,99 @@ class Projet extends \Phalcon\Mvc\Model
         );
     }
 
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getNom()
+	{
+		return $this->nom;
+	}
+
+	/**
+	 * @param string $nom
+	 */
+	public function setNom($nom)
+	{
+		$this->nom = $nom;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return $this->description;
+	}
+
+	/**
+	 * @param string $description
+	 */
+	public function setDescription($description)
+	{
+		$this->description = $description;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDateLancement()
+	{
+		return $this->dateLancement;
+	}
+
+	/**
+	 * @param string $dateLancement
+	 */
+	public function setDateLancement($dateLancement)
+	{
+		$this->dateLancement = $dateLancement;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDateFinPrevue()
+	{
+		return $this->dateFinPrevue;
+	}
+
+	/**
+	 * @param string $dateFinPrevue
+	 */
+	public function setDateFinPrevue($dateFinPrevue)
+	{
+		$this->dateFinPrevue = $dateFinPrevue;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getIdClient()
+	{
+		return $this->idClient;
+	}
+
+	/**
+	 * @param int $idClient
+	 */
+	public function setIdClient($idClient)
+	{
+		$this->idClient = $idClient;
+	}
 }
--- app/views/user/index.volt
+++ app/views/user/index.volt
@@ -1,2 +1,7 @@
 
-fgdfgd
+<?php
+	foreach($projets as $projet)
+	{
+		echo Phalcon\Tag::linkTo('projet/show/' . $projet->getId(), "<div class='divCategorie'><h1>" .$projet->getNom(). "</h1></div>") ;
+    }
+?>
