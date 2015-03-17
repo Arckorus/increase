328b08caf52b002dbfa1b1e18cbf8374a890e13e
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-17 10:57:24 +0100

    ajout premier bouton

---
+++ app/controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pierre
+ * Date: 17/03/2015
+ * Time: 10:47
+ */
+
+class UserController extends Phalcon\Mvc\Controller {
+
+	public function indexAction()
+	{
+
+	}
+
+}
