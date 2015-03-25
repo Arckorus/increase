dc0860f2a8af380fd672be548a0bb9f1ebaf9ce1
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-25 21:11:33 +0100

    les données : avancement, temps écoulé et jour restant ne sont plus calculé dans le modèle mais dans le controleur, puis mise dans un tableau en parallèle, avec l'idée en commun avec la liste des projets

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -13,8 +13,33 @@
 			$user = User::findFirst($id);
 			$projets = $user->getProjets();
 
+			$donnees = array();
+
+			foreach ($projets as $key => $projet) {
+				// Calcul du nombre de jour restant.
+				$donnees[$key]['jourRestant'] = (new DateTime(date("d-m-Y")))->diff(new DateTime($projet->getDateFinPrevue()));
+
+				// Calcul de l'avancement du projet, en %.
+				$poidsTotal = 0;
+				foreach ($projet->getUsecase() as $usecase) {
+					$poidsTotal += $usecase->getPoids();
+				}
+				$avancement = 0;
+				foreach ($projet->getUsecase() as $usecase) {
+					$avancement += (($usecase->getPoids() * 100 / $poidsTotal) / 100) * $usecase->getAvancement();
+				}
+				$donnees[$key]['avancement'] = round($avancement);
+
+				// Calcul du temps écoulé, en %.
+				$nbJourTotal = floor(abs(strtotime($projet->getDateLancement()) - strtotime($projet->getDateFinPrevue())) / (60 * 60 * 24));
+				$nbJourEcoule = floor(abs(strtotime($projet->getDateLancement()) - strtotime(date("d-m-Y"))) / (60 * 60 * 24));
+				$donnees[$key]['tempEcoule'] = round(($nbJourEcoule * 100) / $nbJourTotal);
+			}
+
+
 			$this->view->setVar("user", $user);
 			$this->view->setVar("projets", $projets);
+			$this->view->setVar("donnees", $donnees);
 
 			foreach ($projets as $projet) {
 				$this->jquery->getAndBindTo("#btnProjet" . $projet->getId(), "click", "user/projet/" . $projet->getId(), "#detailProjet");
--- app/models/Projet.php
+++ app/models/Projet.php
@@ -39,12 +39,6 @@ class Projet extends \Phalcon\Mvc\Model
      */
     public $idClient;
 
-	public $jourRestant;
-
-	public $avancement;
-
-	public $tempsEcoule;
-
 	public function initialize()
 	{
 		$this->hasMany("id", "Usecase", "idProjet", array("alias" => "usecase"));
@@ -66,27 +60,6 @@ class Projet extends \Phalcon\Mvc\Model
             'idClient' => 'idClient'
         );
     }
-
-	public function afterFetch()
-	{
-		// Calcul du nombre de jour restant.
-		$this->jourRestant = (new DateTime(date("d-m-Y")))->diff(new DateTime($this->dateFinPrevue));
-
-		// Calcul de l'avancement du projet, en %.
-		$poidsTotal = 0;
-		foreach ($this->getUsecase() as $usecase) {
-			$poidsTotal += $usecase->getPoids();
-		}
-		foreach ($this->getUsecase() as $usecase) {
-			$this->avancement += (($usecase->getPoids() * 100 / $poidsTotal) / 100) * $usecase->getAvancement();
-		}
-		$this->avancement = round($this->avancement);
-
-		// Calcul du temps écoulé, en %.
-		$nbJourTotal = floor(abs(strtotime($this->dateLancement) - strtotime($this->dateFinPrevue)) / (60 * 60 * 24));
-		$nbJourEcoule = floor(abs(strtotime($this->dateLancement) - strtotime(date("d-m-Y"))) / (60 * 60 * 24));
-		$this->tempsEcoule = round(($nbJourEcoule * 100) / $nbJourTotal);
-	}
 
 	/**
 	 * @return int
@@ -166,54 +139,6 @@ class Projet extends \Phalcon\Mvc\Model
 	public function setDateFinPrevue($dateFinPrevue)
 	{
 		$this->dateFinPrevue = $dateFinPrevue;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function getJourRestant()
-	{
-		return $this->jourRestant;
-	}
-
-	/**
-	 * @param mixed $jourRestant
-	 */
-	public function setJourRestant($jourRestant)
-	{
-		$this->jourRestant = $jourRestant;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function getAvancement()
-	{
-		return $this->avancement;
-	}
-
-	/**
-	 * @param mixed $avancement
-	 */
-	public function setAvancement($avancement)
-	{
-		$this->avancement = $avancement;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function getTempsEcoule()
-	{
-		return $this->tempsEcoule;
-	}
-
-	/**
-	 * @param mixed $tempsEcoule
-	 */
-	public function setTempsEcoule($tempsEcoule)
-	{
-		$this->tempsEcoule = $tempsEcoule;
 	}
 
 	/**
